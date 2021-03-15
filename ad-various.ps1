b0e44d60b9db7e519a15b62d2930608f217ab2a7
George Abraham <37941352+georgeabr@users.noreply.github.com>
2021-03-15 13:42:52 +0000

    Update ad-various.ps1

--- ad-various.ps1
+++ ad-various.ps1
@@ -1,11 +1,9 @@
 # get distribution group message delivery restrictions:
 # https://docs.microsoft.com/en=us/exchange/recipients/user=mailboxes/message=delivery=restrictions?view=exchserver=2019
-
 # export to a text file
 Get-DistributionGroup AllMembers | Format-Table AcceptMessagesOnlyFrom,AcceptMessagesOnlyFromDLMembers,RejectMessagesFrom,RejectMessagesFromDLMembers,RequireSenderAuthenticationEnabled -AutoSize | Out-File -Width 1999 out.txt
 
 Get-DistributionGroup AllPeople | Format-list acceptmessagesonlyfrom
-
 # import the text file in Excel, paste special = tick Transpose (horizontal list will be pasted as vertical list, one item per row)
 
 # to add or remove permissions to the distribution group for an email account:
