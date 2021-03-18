d1d91ac34c2b1fb4eb4f96d404c484053ac51223
George Abraham <37941352+georgeabr@users.noreply.github.com>
2021-03-18 10:55:48 +0000

    Create enable-bitlocker.ps1

---
+++ windows-deploy/enable-bitlocker.ps1
@@ -0,0 +1,16 @@
+# enable Bitlocker on drive C and save key on a network share
+
+Import-Module bitlocker
+$DateInt = ((Get-Date).ToString('yyyyMMddHHmm'))
+Enable-BitLocker -MountPoint C: -UsedSpaceOnly -EncryptionMethod Aes256 -TpmProtector -SkipHardwareTest #-WhatIf
+
+$Cred = $(New-Object -Type 'PSCredential' -Arg 'CORP\djoin',$(ConvertTo-SecureString '9qJ+68sv9ht48PND' -AsPlain -Force))
+New-PSDrive -Name "T" -Root "\\192.168.113.10\Teams" -Persist -PSProvider FileSystem -Credential $Cred
+
+$PCSerialNumber = (Get-WmiObject -Class Win32_Bios).SerialNumber
+$BitlockerKeyPath = -join("T:\IT\bitlocker-recovery-keys\", $PCSerialNumber, "-", $DateInt)
+New-Item -ItemType Directory $BitlockerKeyPath
+
+Add-BitLockerKeyProtector -MountPoint C: -RecoveryKeyPath $BitlockerKeyPath -RecoveryKeyProtector
+
+Remove-PSDrive -Name "T"
