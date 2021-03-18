4492e5aff3d5d664e51a9b15641fea0e9d9b370b
George Abraham <37941352+georgeabr@users.noreply.github.com>
2021-03-18 10:58:14 +0000

    Update login-to-vpn.ps1

--- windows-deploy/login-to-vpn.ps1
+++ windows-deploy/login-to-vpn.ps1
@@ -1,4 +1,6 @@
-﻿Add-Type -AssemblyName PresentationFramework
+# check if VPN is started (IP address is pingable), before exiting 
+
+Add-Type -AssemblyName PresentationFramework
 
 function test_connection_ipv4($ipv4) { if (test-connection $ipv4 -Count 1 -ErrorAction SilentlyContinue ) {10} else {20} }
 
@@ -9,7 +11,7 @@ while ($testIP -eq 20)
 {
     Write-Output "not connected"
     [System.Windows.MessageBox]::Show('Please log in with your credentials to the VPN and press OK.
-This is needed for adding the computer to the NFRN domain.','Please log in to the Barracuda VPN first','OK','Warning')
+This is needed for adding the computer to the AD domain.','Please log in to the Barracuda VPN first','OK','Warning')
     $testIP = test_connection_ipv4($IP)
 }
 
@@ -20,7 +22,7 @@ Write-Output "connected"
 The computer will:
 - activate its Windows license
 - be renamed to its serial number
-- join the NFRN domain.
+- join the AD domain.
 
 Afterwards it will automatically reboot.
 You can then proceed to "Software install."','Starting initial setup','OK','Information')
