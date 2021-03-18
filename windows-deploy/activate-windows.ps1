596f3ccead924fba41a499c8cf3320f9bdbb4a1f
George Abraham <37941352+georgeabr@users.noreply.github.com>
2021-03-18 10:54:34 +0000

    Update activate-windows.ps1

--- windows-deploy/activate-windows.ps1
+++ windows-deploy/activate-windows.ps1
@@ -1,1 +1,7 @@
+# activate Windows based on product key embedded in the PC BIOS.
+powercfg /h off
 
+$ProductKey = (Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductKey
+Invoke-Expression "cscript /b C:\Windows\System32\slmgr.vbs -ipk $ProductKey" 
+Start-Sleep 5
+Invoke-Expression "cscript /b C:\Windows\System32\slmgr.vbs -ato"
