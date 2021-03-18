06ac3a997fe9fc65a722c2206f14ae8aa22f39bb
George Abraham <37941352+georgeabr@users.noreply.github.com>
2021-03-18 11:18:35 +0000

    Create initial-setup.cmd

---
+++ windows-deploy/initial-setup.cmd
@@ -0,0 +1,7 @@
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\login-to-vpn.ps1
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\activate-windows.ps1
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\enable-bitlocker.ps1
+
+C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\rename-join-computer.ps1
