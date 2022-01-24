a323e52e6f535a41d83fa413f79429bc6d5cc5fb
George Abraham <37941352+georgeabr@users.noreply.github.com>
2022-01-24 15:34:35 +0000

    Create get-azureAD-license-details.ps1

---
+++ get-azureAD-license-details.ps1
@@ -0,0 +1,4 @@
+$userUPN="user.name@example.com"
+$licensePlanList = Get-AzureADSubscribedSku
+$userList = Get-AzureADUser -ObjectID $userUPN | Select -ExpandProperty AssignedLicenses | Select SkuID 
+$userList | ForEach { $sku=$_.SkuId ; $licensePlanList | ForEach { If ( $sku -eq $_.ObjectId.substring($_.ObjectId.length - 36, 36) ) { Write-Host $_.SkuPartNumber } } }
