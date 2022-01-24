75e278f6b31eec278a3105c2ce41afb235c4a14b
George Abraham <37941352+georgeabr@users.noreply.github.com>
2022-01-24 15:05:38 +0000

    Update enable-bitlocker.ps1

--- windows-deploy/enable-bitlocker.ps1
+++ windows-deploy/enable-bitlocker.ps1
@@ -5,10 +5,10 @@ $DateInt = ((Get-Date).ToString('yyyyMMddHHmm'))
 Enable-BitLocker -MountPoint C: -UsedSpaceOnly -EncryptionMethod Aes256 -TpmProtector -SkipHardwareTest #-WhatIf
 
 $Cred = $(New-Object -Type 'PSCredential' -Arg 'CORP\djoin',$(ConvertTo-SecureString 'password' -AsPlain -Force))
-New-PSDrive -Name "T" -Root "\\192.168.113.10\Teams" -Persist -PSProvider FileSystem -Credential $Cred
+New-PSDrive -Name "T" -Root "\\10.10.10.11\Folder" -Persist -PSProvider FileSystem -Credential $Cred
 
 $PCSerialNumber = (Get-WmiObject -Class Win32_Bios).SerialNumber
-$BitlockerKeyPath = -join("T:\IT\bitlocker-recovery-keys\", $PCSerialNumber, "-", $DateInt)
+$BitlockerKeyPath = -join("T:\bitlocker-recovery-keys\", $PCSerialNumber, "-", $DateInt)
 New-Item -ItemType Directory $BitlockerKeyPath
 
 Add-BitLockerKeyProtector -MountPoint C: -RecoveryKeyPath $BitlockerKeyPath -RecoveryKeyProtector
