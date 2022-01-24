ad4faaf37a96b337fe42b073492363924eb1ed21
George Abraham <37941352+georgeabr@users.noreply.github.com>
2022-01-24 15:37:20 +0000

    Create get-azureAD-sku-license-details.ps1

---
+++ get-azureAD-sku-license-details.ps1
@@ -0,0 +1,9 @@
+$allSKUs=Get-AzureADSubscribedSku
+$licArray = @()
+for($i = 0; $i -lt $allSKUs.Count; $i++)
+{
+$licArray += "Service Plan: " + $allSKUs[$i].SkuPartNumber
+$licArray +=  Get-AzureADSubscribedSku -ObjectID $allSKUs[$i].ObjectID | Select -ExpandProperty ServicePlans
+$licArray +=  ""
+}
+$licArray
