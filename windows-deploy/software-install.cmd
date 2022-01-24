c7146d8d8fa2716cba0e530d2c73d43dcf011bf1
George Abraham <37941352+georgeabr@users.noreply.github.com>
2022-01-24 15:14:11 +0000

    Update software-install.cmd

--- windows-deploy/software-install.cmd
+++ windows-deploy/software-install.cmd
@@ -4,7 +4,9 @@ C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypas
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\eset-av-agent.ps1
 
-start /wait C:\kits\filemaker\base-install\Files\setup.exe /s /passive
-start /wait msiexec.exe /p C:\kits\filemaker\update-15.0.5\FMP.15.0.5.500.Updater.msp /passive
+rem install the software
+start /wait C:\kits\software\base-install\Files\setup.exe /s /passive
+rem install the patch, to update the software to a higher version
+start /wait msiexec.exe /p C:\kits\software\update-15.0.5\FMP.15.0.5.500.Updater.msp /passive
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\phase-2-final-message.ps1
