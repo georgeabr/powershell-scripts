3199828484762ce3ba895bda6959f04d8c8e5616
George Abraham <37941352+georgeabr@users.noreply.github.com>
2022-01-24 15:06:42 +0000

    Update activate-windows.ps1

--- windows-deploy/activate-windows.ps1
+++ windows-deploy/activate-windows.ps1
@@ -1,6 +1,7 @@
-# activate Windows based on product key embedded in the PC BIOS.
+# disable hibernation file, which is the size of the PC's RAM - saves disk space
 powercfg /h off
 
+# activate Windows based on product key embedded in the PC BIOS.
 $ProductKey = (Get-WmiObject -Class SoftwareLicensingService).OA3xOriginalProductKey
 Invoke-Expression "cscript /b C:\Windows\System32\slmgr.vbs -ipk $ProductKey" 
 Start-Sleep 5
