01255b3bda2bd377ec977c9155d52125cb310564
George Abraham <37941352+georgeabr@users.noreply.github.com>
2022-01-24 15:15:31 +0000

    Update initial-setup.cmd

--- windows-deploy/initial-setup.cmd
+++ windows-deploy/initial-setup.cmd
@@ -1,3 +1,4 @@
+rem use full path, safest
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\login-to-vpn.ps1
 
 C:\Windows\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy Bypass -NoLogo -File C:\Users\user\Documents\windows-cust\scripts\activate-windows.ps1
